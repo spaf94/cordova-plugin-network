18335dd9dfe0756f444b83504ee6f16d617db526
Simão Ferreira <spaf94@outlook.pt>
2018-05-14 23:03:39 +0100

    Added android native project

---
+++ native/android/sfnetworklib/src/main/java/com/dev/spaf94/sfnetworklib/SFNetwork.java
@@ -0,0 +1,32 @@
+package com.dev.spaf94.sfnetworklib;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class SFNetwork {
+
+    public boolean haveNetworkConnection(Context context) {
+        if(context != null) {
+            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+            if (connectivityManager != null) {
+                NetworkInfo[] networkInfo = connectivityManager.getAllNetworkInfo();
+
+                if (networkInfo != null && networkInfo.length > 0) {
+                    for (NetworkInfo ni : networkInfo) {
+                        String name = ni.getTypeName();
+
+                        if (name.equalsIgnoreCase("WIFI") || name.equalsIgnoreCase("MOBILE")) {
+                            if (ni.isConnected()) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
