3a82d0af6e52f992ae0952b7cfb10058b96788cd
Simão Ferreira <spaf94@outlook.pt>
2018-05-21 16:18:38 +0100

    Fixed BroadcastReceiver for network to work with all api versions

--- native/android/app/src/main/java/com/dev/spaf94/sfnetworkapp/MainActivity.java
+++ native/android/app/src/main/java/com/dev/spaf94/sfnetworkapp/MainActivity.java
@@ -3,11 +3,37 @@ package com.dev.spaf94.sfnetworkapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.dev.spaf94.sfnetworklib.SFNetwork;
+
 public class MainActivity extends AppCompatActivity {
+
+    SFNetwork sfNetwork;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.sfNetwork = new SFNetwork();
+        this.sfNetwork.startNetworkMonitor(this);
+    }
+
+    /*
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getExtras() != null){
+                boolean haveConnection = sfNetwork.haveNetworkConnection(context);
+                sfNetwork.onConnectionStateChanged(haveConnection);
+            }
+        }
+    };
+    */
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        this.sfNetwork.stopNetworkMonitor(this);
     }
 }
--- native/android/sfnetworklib/src/main/java/com/dev/spaf94/sfnetworklib/SFNetwork.java
+++ native/android/sfnetworklib/src/main/java/com/dev/spaf94/sfnetworklib/SFNetwork.java
@@ -1,10 +1,17 @@
 package com.dev.spaf94.sfnetworklib;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
 
 public class SFNetwork {
+
+    IntentFilter intentFilter;
+    BroadcastReceiver broadcastReceiver;
 
     public boolean haveNetworkConnection(Context context) {
         if(context != null) {
@@ -28,6 +35,28 @@ public class SFNetwork {
         }
 
         return false;
+    }
+
+    public void startNetworkMonitor(AppCompatActivity appCompatActivity){
+        this.intentFilter = new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE");
+
+        this.broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if(intent.getExtras() != null){
+                    boolean haveConnection = haveNetworkConnection(context);
+                    onConnectionStateChanged(haveConnection);
+                }
+            }
+        };
+
+        appCompatActivity.registerReceiver(this.broadcastReceiver, this.intentFilter);
+    }
+
+    public void stopNetworkMonitor(AppCompatActivity appCompatActivity){
+        if(this.broadcastReceiver != null){
+            appCompatActivity.unregisterReceiver(this.broadcastReceiver);
+        }
     }
 
     public void onConnectionStateChanged(boolean haveConnection){
