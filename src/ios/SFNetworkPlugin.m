e183a532c773e72d2522cea4432cc8edad0517b7
Simão Ferreira <spaf94@outlook.pt>
2018-06-20 13:58:34 -0700

    Method haveNetworkConnection for iOS

--- src/ios/SFNetworkPlugin.m
+++ src/ios/SFNetworkPlugin.m
@@ -1,16 +1,27 @@
 #import "SFNetworkPlugin.h"
+#include <unistd.h>
+#include <netdb.h>
 
 @implementation SFNetworkPlugin
 
 - (void)haveNetworkConnection:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
-    NSString* echo = @"hello";//[command.arguments objectAtIndex:0];
 
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
+    char *hostname;
+    struct hostent *hostinfo;
+    hostname = "google.com";
+    hostinfo = gethostbyname(hostname);
+
+    if (hostinfo == NULL)
+    {
+        //NSLog(@"-> no connection!\n");
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+    else
+    {
+        //NSLog(@"-> connection established!\n");
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"OK"];
     }
 
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
